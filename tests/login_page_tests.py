e778032dabbb8a2b4f4ffafb27fa3aa6c33ae652
Alexander <GeseltinAV@gmail.com>
2020-10-30 17:35:03 +0300

    Новая ветка Локаторы переехали в модуль locators.py

--- tests/login_page_tests.py
+++ tests/login_page_tests.py
@@ -43,14 +43,14 @@ def test_invalid_login(app):
 
     # Check if login page is presented
     actual_title = app.driver.title
-    if actual_title.lower() is not locators._login_page_title.lower():
+    if actual_title.lower() != locators._login_page_title.lower():
         app.driver.find_element_by_xpath(locators._logout_button).click()
 
     # Clearing fields
     app.wait.until(EC.presence_of_element_located((By.XPATH, locators._login_field))).clear()
     app.wait.until(EC.presence_of_element_located((By.XPATH, locators._password_field))).clear()
 
-    # Entering wrong credentials
+    # Entering wrong login
     app.wait.until(EC.presence_of_element_located((By.XPATH, locators._login_field))).send_keys(_login)
 
     # enter password
@@ -66,17 +66,44 @@ def test_invalid_login(app):
     time.sleep(3)
 
 
-# def test_invalid_password(app):
-#     pass
-#
-#
-# def test_empty_fields(app):
-#     pass
-#
-#
-# def test_empty_login_field(app):
-#     pass
-#
-#
-# def test_empty_password_field(app):
-#     pass
+def test_invalid_password(app):
+    # Credentials
+    _login = "Administrator"
+    _password = "Qwe12345"
+    # Expected error message
+    _expected_error_message = "Извините, пользователя с таким логином и паролем найти не удалось. Попробуйте ещё раз."
+
+    # Check if login page is presented
+    actual_title = app.driver.title
+    if actual_title.lower() != locators._login_page_title.lower():
+        app.driver.find_element_by_xpath(locators._logout_button).click()
+
+    # Clearing fields
+    app.wait.until(EC.presence_of_element_located((By.XPATH, locators._login_field))).clear()
+    app.wait.until(EC.presence_of_element_located((By.XPATH, locators._password_field))).clear()
+
+    # Entering login
+    app.wait.until(EC.presence_of_element_located((By.XPATH, locators._login_field))).send_keys(_login)
+
+    # Entering wrong password
+    for i in _password:
+        app.driver.find_element_by_xpath(locators._password_field).send_keys(i)
+        time.sleep(0.5)
+    # click login button
+    app.driver.find_element_by_xpath(locators._login_button).click()
+
+    # Check result
+    actual_result = app.driver.find_element_by_xpath(locators._error_field).text
+    assert _expected_error_message.lower() in actual_result.lower()
+    time.sleep(3)
+
+
+def test_empty_fields(app):
+    # Check if login page is presented
+    actual_title = app.driver.title
+    if actual_title.lower() != locators._login_page_title.lower():
+        app.driver.find_element_by_xpath(locators._logout_button).click()
+
+    # Check if login button is disabled
+    element_state = app.wait.until(EC.element_to_be_clickable((By.XPATH, locators._login_button)))
+    assert element_state
